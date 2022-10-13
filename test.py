5be3a9e6a59dcf7a3caa4eca7337ea926a3b4106
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2022-10-13 20:49:23 +0200

    testing new CSV format

--- test.py
+++ test.py
@@ -29,7 +29,11 @@ for category in categories:
         with open(file, encoding='utf-8') as f:
             i = 0
             for line in csv.reader(f, delimiter=";"):
+                if not i == 0:
+                    assert len(line) == 4, "Line {} should contain 4 elements in {}".format(i, file)
+                    assert is_proper_coordinate(line[2]), "Line {} of {} latitude should be a proper coordinate".format(i, file)
+                    assert is_proper_coordinate(line[3]), "Line {} of {} longitude should be a proper coordinate".format(i, file)
+                else:
+                    assert ";".join(line) == "name;location;longitude;latitude", "First line in {} file should be a header row".format(file)
+
                 i = i + 1
-                assert len(line) == 4, "Line {} should contain 4 elements in {}".format(i, file)
-                assert is_proper_coordinate(line[2]), "Line {} latitude should be a proper coordinate".format(i, file)
-                assert is_proper_coordinate(line[3]), "Line {} longitude should be a proper coordinate".format(i, file)
